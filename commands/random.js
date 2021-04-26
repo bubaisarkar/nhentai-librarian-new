350e2f9458d73f61cf951d1001f27c5d149581d9
bubaisarkar <83076624+bubaisarkar@users.noreply.github.com>
2021-04-26 10:00:42 +0530

    Update random.js

--- commands/random.js
+++ commands/random.js
@@ -1,8 +1,4 @@
 exports.run = async (client, msg, args, color) => {
-  if (!msg.channel.nsfw)
-    return msg.channel
-      .send(`Nhentai Librarian Needs NSFW channel`)
-      .then(msg => msg.delete({ timeout: 5000 }));
   let res = await client.embeds.getRandom();
   await client.embeds.getInfoEmbed(res.id, msg);
 };

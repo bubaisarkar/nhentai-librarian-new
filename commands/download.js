935ae9b41f19522b7a02f243367c56172e05e951
bubaisarkar <83076624+bubaisarkar@users.noreply.github.com>
2021-04-26 09:58:07 +0530

    Update download.js

--- commands/download.js
+++ commands/download.js
@@ -1,10 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, msg, args, color) => {
-  if (!msg.channel.nsfw)
-    return msg.channel
-      .send(`Nhentai Librarian Needs NSFW channel`)
-      .then(msg => msg.delete({ timeout: 5000 }));
   let res = await client.embeds.getById(args[0]);
   let type = args[1];
   let embed = await client.embeds.download(res, type);
